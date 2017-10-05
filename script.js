c43299170c8994bd14a6962c5a7c0cc3ca134ad8
Steven Barra <stevenbarra@gmail.com>
2017-10-04 19:42:19 -0700

    Apparently modified files and added img folder

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
 $(document).ready(function(){
+  var sForm = document.getElementById('simple');
+  var state;
+$(sForm.lastElementChild).mousedown(function(){
+  $(".merlin-icon").addClass('.flipped');
+$(sForm.lastElementChild).mouseup(function(){
+  $(".merlin-icon").removeClass('.flipped');
+  console.log(sForm.val())
+  if(sForm.city.value){
+      var cities =  sForm.city.value;
+  }
+  console.log('stored var', cities)
 //   //retrieve our location info
-   let locationData = $.get('https://ipapi.co/json/');
+   var locationData = $.get('https://ipapi.co/json/');
     var temp = document.querySelector(".temp");
     var myCity = document.querySelector(".city");
     var myState = document.querySelector(".state");
-    var weatherIcon = document.querySelector(".weather-icon");
+    var merlinIcon = document.querySelector(".merlin-icon");
    console.log(locationData);
 
 //   //Make sure to declare variables to store data
 
     locationData.then(function(locationAPI, status){
-//     console.log(locationAPI.city);
+     console.log(locationAPI.city);
     let lat = locationAPI.latitude;
     let lon = locationAPI.longitude;
-    let city = locationAPI.city;
-    let state = locationAPI.region;
+    let city = cities;
     getWeatherData(lat, lon, city, state);
 
     console.log(`The longitude is: ${lat}`);
@@ -25,14 +35,21 @@ $(document).ready(function(){
   function getWeatherData(lat, lon, city, state) {
     var weatherAPI = 'https://api.openweathermap.org/data/2.5/weather?';
     var apiKEY = 'ac3aaf635fa8ccdf51fac960eec79daf';
-    var weatherData = $.getJSON(`${weatherAPI}lat=${lat}&lon=${lon}&APPID=${apiKEY}`);
+    var weatherData = $.getJSON(`${weatherAPI}q=${city}&appid&APPID=${apiKEY}`);
 
 
     weatherData.then(function(response, status){
       let myTemp = response.main.temp;
-      temp.textContent = Math.round(myTemp);
+      temp.textContent = Math.round(myTemp * (9/5) - 459.67);
+      myCity.innerHTML = response.name;
+      console.log(myCity.innerHTML)
+      myState.innerHTML = response.sys.country;
+
+
       // Insert your data into the html! hint: log the api response and see what data is available.
-      console.log(response);
+      console.log('RESPONSE',response);
     });
-   }
+  }
+    });
+  });
 });
